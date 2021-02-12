ccdc35404b3e2dd076d07af887a49876db5cfc5d
Jefferson Nuñez <jecfmaatrixxx@gmail.com>
2021-02-11 23:48:29 -0500

    Update RegistrarCuenta.java

--- src/Domain/CasosDeUso/RegistrarCuenta.java
+++ src/Domain/CasosDeUso/RegistrarCuenta.java
@@ -1,15 +1,45 @@
 package Domain.CasosDeUso;
 
 import Domain.Entidades.Cuenta;
+import Domain.ILogeoXUsuario;
+import Persistencia.IOperacionDBCuenta;
 
+public class RegistrarCuenta implements ILogeoXUsuario {
 
-public class RegistrarCuenta extends IniciarCuenta {
-    public void registrarCuenta(Cuenta cuenta) {
-        if (cuenta.comprobarExistenciaUser(cuenta.getUsuario(), cuenta.getCorreo())) {
-            System.out.println("Datos enviados correctamente :" + cuenta.getNombre() + " - " + cuenta.getUsuario() + " - " + cuenta.getCorreo() + "");
-        } else {
-            System.out.println("El email o login ya ha sido registrado. Por favor escriba otros datos");
+    IOperacionDBCuenta operacionCuenta;
+
+    public RegistrarCuenta(IOperacionDBCuenta operacionCuenta) {
+        this.operacionCuenta = operacionCuenta;
+    }
+
+    @Override
+    public void Logear(Cuenta cuenta) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public int Registrar(Cuenta cuenta) {
+        Cuenta objCue = cuenta;
+
+        if (!VerificarCorreoExistente(cuenta.getUsuario(), cuenta.getCorreo())) {
+            //Insertar cuenta
+            Persistencia.Cuenta objCuenrepo = new Persistencia.Cuenta();
+            objCuenrepo.setNombre(cuenta.getNombre());
+            objCuenrepo.setApellido(cuenta.getApellido());
+            objCuenrepo.setCorreo(cuenta.getCorreo());
+            objCuenrepo.setUsuario(cuenta.getUsuario());
+            objCuenrepo.setContraseña(cuenta.getContraseña());
+
+            objCuenrepo.setId(1);
+            operacionCuenta.InsertarCuenta(objCue);
+            return 1;
         }
+        return -1;
+    }
+
+    @Override
+    public boolean VerificarCorreoExistente(String user, String correo) {
+        return operacionCuenta.ComprobarExistencia(user, correo);
     }
 
 }
