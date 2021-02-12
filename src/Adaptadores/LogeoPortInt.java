6d2c9c4fc35dcbd46732dbee01a905e6ae094f02
Jefferson Nuñez <jecfmaatrixxx@gmail.com>
2021-02-11 23:40:06 -0500

    Update LogeoPortInt.java

--- src/Adaptadores/LogeoPortInt.java
+++ src/Adaptadores/LogeoPortInt.java
@@ -2,16 +2,25 @@ package Adaptadores;
 
 import Domain.ILogeoXUsuario;
 import Domain.Entidades.Cuenta;
+import Persistencia.IOperacionDBCuenta;
 
 import Persistencia.RepositorioCuenta;
 
 public class LogeoPortInt {
+
+    ILogeoXUsuario logeoXUsuario;
+    IOperacionDBCuenta operacionCuenta;
+
     public RepositorioCuenta repositorioCuenta;
 
-    public void LogeoXUsuario(ILogeoXUsuario logeo, RepositorioCuenta repositorioCuenta) {
+    public LogeoPortInt(ILogeoXUsuario iLogeoXUsuario, IOperacionDBCuenta operacionCuenta) {
+        this.logeoXUsuario = iLogeoXUsuario;
+        this.operacionCuenta = operacionCuenta;
     }
 
-    public void RegistrarCuenta(Cuenta cuenta) {
+    public int RegistrarCuenta(Cuenta cuenta) {
+        return this.logeoXUsuario.Registrar(cuenta);
+
     }
 
 }
